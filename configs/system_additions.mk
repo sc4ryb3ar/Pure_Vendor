80f6ca59ff07fd11345716a9e924560d83c4fe21
Steve Kondik <shade@chemlab.org>
2010-07-10 19:26:43 -0400

    [vendor/pure] Add sysinit script. *also remove logwrapper *add system init service to init.purenexus.rc
    Change-Id: Iff4a08dedff4bf4b80471bbc68320e6962ecc062
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -22,6 +22,14 @@ PRODUCT_COPY_FILES += \
     vendor/pure/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/pure/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh
 
+# init.d script support
+PRODUCT_COPY_FILES += \
+    vendor/pure/prebuilt/bin/sysinit:system/bin/sysinit
+
+# Pure-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/pure/prebuilt/etc/init.purenexus.rc:root/init.purenexus.rc
+
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
