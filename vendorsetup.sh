rmnhg/android_vendor_pure

Add osprey, A6020 & surnia

vendorsetup.sh
@@ -1,2 +1,5 @@
 add_lunch_combo kenzo-userdebug
 add_lunch_combo hydrogen-userdebug
+add_lunch_combo osprey-userdebug
+add_lunch_combo surnia-userdebug
+add_lunch_combo A6020-userdebug

sc4ryb3ar/Pure_Vendor

vendorsetup.sh
@@ -1,1 +1,4 @@
 add_lunch_combo kenzo-user
+add_lunch_combo osprey-user
+add_lunch_combo surnia-user
+add_lunch_combo A6020-user