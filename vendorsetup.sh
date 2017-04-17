rmnhg/android_vendor_pure

Add lux to the party

vendorsetup.sh
@@ -5,3 +5,4 @@ add_lunch_combo surnia-userdebug
 add_lunch_combo A6020-userdebug
 add_lunch_combo victara-userdebug
 add_lunch_combo condor-userdebug
+add_lunch_combo lux-userdebug

sc4ryb3ar/Pure_Vendor

vendorsetup.sh
@@ -2,3 +2,4 @@ add_lunch_combo kenzo-user
 add_lunch_combo osprey-user
 add_lunch_combo surnia-user
 add_lunch_combo A6020-user
+add_lunch_combo lux-user