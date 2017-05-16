a6e48b6fd6df746d36d2d002dfbe1513f8549305
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-05-16 18:12:23 -0300

    Drop A6020

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,6 @@
 add_lunch_combo kenzo-userdebug
 add_lunch_combo osprey-userdebug
 add_lunch_combo surnia-userdebug
-add_lunch_combo A6020-userdebug
 add_lunch_combo lux-userdebug
 add_lunch_combo condor-userdebug
 add_lunch_combo victara-userdebug
