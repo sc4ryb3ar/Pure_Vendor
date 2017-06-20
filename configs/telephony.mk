a66c5670f898aea90cf5c9ba19e7f3866b9fe369
Eliminater74 <eliminater74@gmail.com>
2017-06-19 22:24:24 -0400

    [vendor/pure] telephony: Add: sensitive_pn/selective-spn-conf
    Change-Id: I6f51752f6aceee194316b3660778fc9a790b3cd6
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- configs/telephony.mk
+++ configs/telephony.mk
@@ -9,3 +9,11 @@ PRODUCT_BOOT_JARS += \
 # World APN list
 PRODUCT_COPY_FILES += \
     vendor/pure/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/pure/prebuilt/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml  
+    
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/pure/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml     
