4a27be6f8b86eb4a67fe9980768f099c50dc0c8e
Aren Clegg <aclegg2011@gmail.com>
2017-07-22 09:33:56 -0700

    vendor/pure: Add axon7 device to vendor/pure
    Change-Id: If70b41be9a5fb55058bc9cddf48e7df1b38c67a8
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

---
+++ products/axon7.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2017 Pure Fusion OS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Inherit AOSP device configuration for pme
+$(call inherit-product, device/htc/pme/aosp_pme.mk)
+
+# Set those variables here to overwrite the inherited values.
+
+PRODUCT_DEVICE := axon7
+PRODUCT_NAME := axon7
+PRODUCT_BRAND := ZTE
+PRODUCT_MODEL := ZTE AXON 7
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_RELEASE_NAME := axon7
+
+TARGET_VENDOR := ZTE
+
