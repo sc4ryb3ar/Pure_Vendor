b225be6c3dda38f5b05b398f07b31a4b37e8a9c7
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:04:34 +0200

    [vendor/pure] soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
    * Soong product variable 'has_legacy_camera_hal1'
    
    Change-Id: I10800cba0c431297b5d7aad6e97d57a01232d73e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -2,9 +2,13 @@ package android
 type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
+}
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
 	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,5 +5,6 @@ pure_soong:
 	echo '{'; \
 	echo '"Pure": {'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
