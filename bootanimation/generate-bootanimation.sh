993e9e7646fba6f8792fb7f91c7f0730f1080c12
siankatabg <siankata91@gmail.com>
2017-08-12 17:32:09 +0100

    Vendor/Pure: BootAnimation: Introduce newest Official Boot Animation By Roger
    Change-Id: Ic4761a8f828afd8d5ad732f176934754c7525381

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -4,7 +4,6 @@ WIDTH="$1"
 HEIGHT="$2"
 HALF_RES="$3"
 OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
-RANDOM_BOOT=$(shuf -i 0-9 -n 1)
 
 if [ -z "$WIDTH" ]; then
     echo "Warning: bootanimation width not specified"
@@ -18,32 +17,22 @@ if [ -z "$HEIGHT" ]; then
     HEIGHT="2560"
 fi
 
-if [ "$HEIGHT" -lt "$WIDTH" ]; then
-    IMAGEWIDTH="$HEIGHT"
-else
-    IMAGEWIDTH="$WIDTH"
-fi
-
-IMAGESCALEWIDTH="$IMAGEWIDTH"
-IMAGESCALEHEIGHT=$(expr $IMAGESCALEWIDTH / 3)
-
 if [ "$HALF_RES" = "true" ]; then
-    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
+    WIDTH=$(expr $WIDTH / 2)
+    HEIGHT=$(expr $HEIGHT / 2)
 fi
 
-IMAGEHEIGHT=$(expr $IMAGEWIDTH / 3)
-
-RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
+RESOLUTION=""$WIDTH"x"$HEIGHT""
 
 for part_cnt in 0 1 2 3 4
 do
     mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
 done
-tar xfp "vendor/pure/bootanimation/bootanimation$RANDOM_BOOT.tar" -C "$OUT/bootanimation/"
-mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
+tar xfp "vendor/pure/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/"
+mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".jpg"
 
 # Create desc.txt
-echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT" 60 > "$OUT/bootanimation/desc.txt"
+echo "$WIDTH $HEIGHT" 15 > "$OUT/bootanimation/desc.txt"
 cat "vendor/pure/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
 # Create bootanimation.zip
