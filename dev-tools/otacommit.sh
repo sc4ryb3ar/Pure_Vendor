3a54dc9855243a560afe85274daa9d76eee83f36
Tab Fitts <tfitts1212@gmail.com>
2017-08-14 11:12:46 -0700

    [vendor/pure] Add changelog to build script.
    Change-Id: If0ea3c32a7341c9bfe1bc693f5f3ea749490710b

--- dev-tools/otacommit.sh
+++ dev-tools/otacommit.sh
@@ -13,10 +13,11 @@ cd $OTAPATH && git reset --hard &&git fetch &&git pull
 
 sed -i -- 's/'"<VersionName"'.\+<\/\VersionName>/'"\<VersionName>$OTA_VERSION<\/\VersionName>"'/g' $OTAXML
 sed -i -- 's/'"$ROMPREFIX\_"'.\+zip/'"$FILENAME"'/g' $OTAXML
+sed -i -- 's/'"$ROMPREFIX\_"'.\+txt/'"$CHANGELOG"'/g' $OTAXML
 sed -i -- 's/'"<FileSize"'.\+<\/\FileSize>/'"\<FileSize type=\"\integer\"\>$FILESIZE<\/\FileSize>"'/g' $OTAXML
 sed -i -- 's/'"<VersionNumber"'.\+<\/\VersionNumber>/'"\<VersionNumber type=\"\integer\"\>$BUILD_DATE<\/\VersionNumber>"'/g' $OTAXML
-sed -i -- 's/'"<CheckMD5"'.\+<\/\CheckMD5>/'"\<CheckMD5>$MD5<\/\CheckMD5>"'/g' $OTAXML
 
+sed -i -- 's/'"<CheckMD5"'.\+<\/\CheckMD5>/'"\<CheckMD5>$MD5<\/\CheckMD5>"'/g' $OTAXML
 echo " "
 echo "Committing and pushing changes"
 
