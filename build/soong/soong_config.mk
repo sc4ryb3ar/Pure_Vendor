2269891453d283440a874dd9de161d4777486a89
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    [vendor/pure] build: soong: put pure variables in the "Pure" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
-lineage_soong:
+# Insert new variables inside the Pure structure
+pure_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Pure": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
