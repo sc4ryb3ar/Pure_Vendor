voidz777-pure/android_vendor_pure

bullhead product

vendorsetup.sh
@@ -1,1 +1,2 @@
 add_lunch_combo nexus_angler-user
+add_lunch_combo nexus_bullhead-user

sc4ryb3ar/Pure_Vendor

vendorsetup.sh
@@ -1,1 +1,2 @@
 add_lunch_combo angler-user
+add_lunch_combo bullhead-user