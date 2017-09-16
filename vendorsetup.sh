blade86dev/android_vendor_pure

products: add marlin product

vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo angler-userdebug
 add_lunch_combo bullhead-userdebug
 add_lunch_combo hammerhead-userdebug
+add_lunch_combo marlin-userdebug
 add_lunch_combo shamu-userdebug

sc4ryb3ar/Pure_Vendor

vendorsetup.sh
@@ -1,2 +1,3 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
+add_lunch_combo marlin-user