rcstar6696/android_vendor_pure

vendor_pure: add tablet config, and dragon product

vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo angler-userdebug
 add_lunch_combo bullhead-userdebug
+add_lunch_combo dragon-userdebug
 add_lunch_combo marlin-userdebug
 add_lunch_combo sailfish-userdebug
 add_lunch_combo shamu-userdebug

sc4ryb3ar/Pure_Vendor

vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo angler-user
 add_lunch_combo bullhead-user
+add_lunch_combo dragon-user
 add_lunch_combo marlin-user
 add_lunch_combo sailfish-user