b3a6284ae915a18fa3efc6cd7bfdfdaf35f63213
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-09-25 11:53:03 +0530

    pure: Rework scripts
    Change-Id: I4f8fec31a1ac5591ff8b499ca0d8b6d8e7aa1c3f
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- dev-tools/otacommit.sh
+++ dev-tools/otacommit.sh
@@ -3,13 +3,18 @@
 ### Little Android Build Script
 ### Copyright 2017, Tab Fitts
 
+# green = confirmations, blue = informational
+CLR_RST=$(tput sgr0)                        ## reset flag
+CLR_BLD_GRN=$CLR_RST$CLR_BLD$(tput setaf 2) #  green, bold
+CLR_BLD_BLU=$CLR_RST$CLR_BLD$(tput setaf 4) #  blue, bold
+
 echo " "
-echo "Updating ota file."
+echo "${CLR_BLD_BLU}Updating OTA XML.${CLR_RST}"
 echo " "
 
 source config.conf
 
-cd $OTAPATH && git reset --hard &&git fetch &&git pull
+cd $OTAPATH && git reset --hard && git fetch && git pull
 
 sed -i -- 's/'"<VersionName"'.\+<\/\VersionName>/'"\<VersionName>$OTA_VERSION<\/\VersionName>"'/g' $OTAXML
 sed -i -- 's/'"$ROMPREFIX\_"'.\+zip/'"$FILENAME"'/g' $OTAXML
@@ -19,8 +24,16 @@ sed -i -- 's/'"<VersionNumber"'.\+<\/\VersionNumber>/'"\<VersionNumber type=\"\s
 
 sed -i -- 's/'"<CheckMD5"'.\+<\/\CheckMD5>/'"\<CheckMD5>$MD5<\/\CheckMD5>"'/g' $OTAXML
 echo " "
-echo "Committing and pushing changes"
+
+# Check if variables are unset or empty
+if [ -z "$FILENAME" ] || [ -z "$MD5SUMNAME" ] || [ -z "$CHANGELOG" ] || [ -z "$FILESIZE" ] || [ -z "$MD5" ] || [ -z "$OTA_VERSION" ] || [ -z "$OTA_NUMBER" ]
+then
+    echo "${CLR_BLD_RED}Some variables are unset or empty. Exiting...${CLR_RST}"
+    exit 1
+fi
+
+echo "${CLR_BLD_BLU}Committing and pushing changes${CLR_RST}"
 
 git commit $OTAXML -m "'$OTACOMMITMSG'" && git push $OTAREPO
 
-echo "Update ota complete."
+echo "${CLR_BLD_GRN}Update of OTA XML complete.${CLR_RST}"
