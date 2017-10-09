20a211890c7dc2df439b010248fd0984edc22808
Tab Fitts <tfitts1212@gmail.com>
2017-10-09 09:52:20 -0700

    [vendor/pure] Add pme target
    Change-Id: Id6a2d258d01d974d957a03f58d7f8295a47ffc5d

---
+++ products/pme.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2017 Pure Fusion OS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Inherit AOSP device configuration for pme
+$(call inherit-product, device/htc/pme/aosp_pme.mk)
+
+# Set those variables here to overwrite the inherited values.
+
+PRODUCT_DEVICE := pme
+PRODUCT_NAME := pme
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := HTC 10
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_RELEASE_NAME := pme
+
+TARGET_VENDOR := htc
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
