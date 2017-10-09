104bf119661f6b9c6fde4b185bb0a28304e63923
Tab Fitts <tfitts1212@gmail.com>
2017-10-09 09:53:49 -0700

    [vendor/pure] Update vendorsetup.sh
    Change-Id: Iff39c5a03c3bf683ea9434a5e35e196efa648c40

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
-add_lunch_combo angler-user
-add_lunch_combo bullhead-user
-add_lunch_combo dragon-user
-add_lunch_combo marlin-user
-add_lunch_combo sailfish-user
+add_lunch_combo pme-userdebug
+add_lunch_combo pme-user
+add_lunch_combo pme-eng
