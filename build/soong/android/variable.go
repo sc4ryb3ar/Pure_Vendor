8acaef807448f700d568e5e52a1ac4e3f590b6c9
Tab Fitts <tfitts1212@gmail.com>
2017-10-10 15:17:38 -0700

    [vendor/pure] Remove some commits for now
    Revert "[vendor/pure] Fix soong makefile"
    
    This reverts commit 36d941e875cacaf07fe7e0f9c8de2e2bfb942cd4.
    
    Revert "[vendor/pure] soong_config: Add TARGET_USES_NON_TREBLE_CAMERA variable"
    
    This reverts commit 93154b92075a6368934f425a9bacf663dc6434fb.
    
    Revert "[vendor/pure] soong_config: Add TARGET_USES_MEDIA_EXTENSIONS variable"
    
    This reverts commit 5bb3811eb500d8638eb79de9dc6486b5dec806f7.
    
    Revert "[vendor/pure] soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable"
    
    This reverts commit b225be6c3dda38f5b05b398f07b31a4b37e8a9c7.
    
    Change-Id: Ib70157949c12689ee78c1cae542d63c250a5f9c7

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -2,23 +2,9 @@ package android
 type Product_variables struct {
 	Needs_text_relocations struct {
 		Cppflags []string
-}
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
-	Uses_non_treble_camera struct {
-		Cflags []string
 	}
 }
 
 type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_non_treble_camera  *bool `json:",omitempty"`
 }
